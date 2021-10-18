b8605c190987577012b1f8305803bfd8798e151c
CharlieS115 <cstorey115@gmail.com>
2021-10-18 14:05:52 +0000

    Added camps.html and folders and testing the queryset works by checking it renders on the camps.html page

--- camps/views.py
+++ camps/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Camp
 
 # Create your views here.
+
+
+def all_camps(request):
+    """ A view to show all upcoming camps """
+
+    camps = Camp.objects.all()
+
+    context = {
+        'camps': camps,
+    }
+
+    return render(request, 'camps/camps.html', context)
