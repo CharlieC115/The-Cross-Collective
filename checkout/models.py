be787f0eaf81ba98a42ff7f0869b885d912f5723
CharlieS115 <cstorey115@gmail.com>
2021-10-22 21:56:08 +0000

    Added forms and signals to the checkout app.

--- checkout/models.py
+++ checkout/models.py
@@ -23,19 +23,16 @@ class Order(models.Model):
         """ Generate a random, uniq order number using uuid """
         return uuid.uuid4().hex.upper()
 
-    
     def update_total(self):
         """ Update total everytime a line item is added """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         self.save()
 
-    
     def save(self, *args, **kwargs):
         """ Override the original save method to set the order number if it hasn't been set already. """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
-
 
     def __str__(self):
         return self.order_number
@@ -47,12 +44,10 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
-
     def save(self, *args, **kwargs):
         """ Override the original save method to set the lineitem total and update the order total. """
         self.lineitem_total = self.camp.price * self.quantity
         super().save(*args, **kwargs)
-
 
     def __str__(self):
         return f'Thank you for booking {self.camp.name}. You order number is {self.order.order_number}'
