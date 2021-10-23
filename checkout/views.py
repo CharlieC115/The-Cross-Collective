54efefd2cac7f5e8e8d3665ed9c1f9ba866a2ac6
CharlieS115 <cstorey115@gmail.com>
2021-10-23 00:40:40 +0000

    Added checkout page complete with basic form fields and validation and order summary. Basic stylin  crispy form installed. Added and links to urls, views, and settings are done.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+
+from .forms import OrderForm
 
 # Create your views here.
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        return redirect(reverse('camps'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
