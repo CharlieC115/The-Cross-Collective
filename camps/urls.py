e81cfead933e87ee80f26c7720953a965117d183
CharlieS115 <cstorey115@gmail.com>
2021-10-26 00:50:28 +0000

    Added a camp details page and styled

--- camps/urls.py
+++ camps/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_camps, name='camps')
+    path('', views.all_camps, name='camps'),
+    path('<camp_id>', views.camp_details, name='camp_details'),
 ]
--- camps/views.py
+++ camps/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Camp
 
 # Create your views here.
@@ -19,3 +19,20 @@ def all_camps(request):
     }
 
     return render(request, 'camps/camps.html', context)
+
+
+def camp_details(request, camp_id):
+    """ A view to show camp details of a selected camp """
+
+    camp = get_object_or_404(Camp, pk=camp_id)
+
+    # if request.GET:
+    #     if 'category' in request.GET:
+    #         categories = request.GET['category'].split(',')
+    #         camps = camps.filter(category__name__in=categories)
+
+    context = {
+        'camp': camp,
+    }
+
+    return render(request, 'camps/camp_details.html', context)
